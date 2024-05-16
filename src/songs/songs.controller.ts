8cdf51a187d6f0c2199dc2c29cb6a6290de51472
Richard <richard.herew@gmail.com>
2024-05-16 21:56:43 +0700

    feat: songs service

--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { CreateSongDTO } from './dto/create-song-dto';
+import { Song } from './schemas/song';
 
 @Controller('songs')
 export class SongsController {
@@ -12,5 +13,26 @@ export class SongsController {
     createSongDTO: CreateSongDTO
   ) {
     return this.songsService.create(createSongDTO);
+  }
+
+  @Get()
+  find(): Promise<Song[]> {
+    return this.songsService.find();
+  }
+
+  @Get(':id')
+  findOne(
+    @Param('id')
+    id: string
+  ): Promise<Song> {
+    return this.songsService.findById(id);
+  }
+
+  @Delete(':id')
+  delete(
+    @Param('id')
+    id: string
+  ) {
+    return this.songsService.delete(id);
   }
 }
